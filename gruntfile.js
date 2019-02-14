6df266c0ed24548f10358656b699c9c2f11b196a
Holger Mueller <9jholger@gmail.com>
2019-02-14 16:06:16 -0600

    minified css

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,22 @@
+'use strict';
 module.exports = grunt => {
+
+  grunt.initConfig({
+
+    cssmin: {
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'public/assets/css',
+          src: ["*.css", '!*.min.css'],
+          dest: 'public/assets/css',
+          ext: '.min.css'
+        }]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 };
